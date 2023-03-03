f26a416d0550d8b7204412b4864a2a86be781203
Tomohiro Matsuo <gacktomo@gmail.com>
2023-03-03 17:11:43 +0900

    Add decisionInput

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import {
   FieldHeight,
   FieldWidth,
@@ -28,6 +28,7 @@ export default function Home() {
     x: 0,
     y: 0,
   });
+  const isReleased = useMemo(() => currentMino.y >= 0, [currentMino]);
 
   const releaseMino = useCallback(() => {
     const minos = Object.values(Mino).filter((mino) => mino !== Mino.None);
@@ -35,74 +36,86 @@ export default function Home() {
     setCurrentMino({ mino, rotation: Rotation.R0, x: ReleasePosition, y: -1 });
   }, []);
 
-  const moveMino = (input: Input) => {
-    const newMinoState = { ...currentMino };
-    switch (input) {
-      case Input.Left:
-        newMinoState.x -= 1;
-        if (currentMino.y < 0) return;
-        if (newMinoState.x < 0) return;
-        break;
-      case Input.Right:
-        newMinoState.x += 1;
-        if (currentMino.y < 0) return;
-        if (newMinoState.x >= FieldWidth) return;
-        break;
-      case Input.RotateLeft:
-        newMinoState.rotation = ((newMinoState.rotation - 1) % 4) as Rotation;
-        break;
-      case Input.RotateRight:
-        newMinoState.rotation = ((newMinoState.rotation + 1) % 4) as Rotation;
-        break;
-      case Input.Down:
-        newMinoState.y += 1;
-        break;
-    }
-    const newField = field.map((row) => row.map((cell) => cell));
-    const shape = minoShapeMap.get(currentMino.mino);
-    if (!shape) return;
-    for (const [y, row] of shape.entries()) {
-      for (const [x, cell] of row.entries()) {
-        if (cell === Mino.None) continue;
-        const newY = newMinoState.y + y;
-        const newX = newMinoState.x + x;
-        if (newY >= FieldHeight || field[newY][newX] !== Mino.None) {
-          if (input !== Input.Down) return;
-          if (currentMino.y < 0) {
-            console.log("Game Over");
-            setField(initField);
+  const decisionInput = useCallback(() => {
+    const actions = [
+      new Array(4).fill(null).map(() => Input.Left),
+      new Array(4).fill(null).map(() => Input.Right),
+      [],
+    ];
+    setInputQueue((prev) => [
+      ...prev,
+      ...actions[Math.round(Math.random() * (actions.length - 1))],
+    ]);
+  }, []);
+
+  const moveMino = useCallback(
+    (input: Input) => {
+      const newMinoState = { ...currentMino };
+      switch (input) {
+        case Input.Left:
+          newMinoState.x -= 1;
+          if (currentMino.y < 0) return;
+          if (newMinoState.x < 0) return;
+          break;
+        case Input.Right:
+          newMinoState.x += 1;
+          if (currentMino.y < 0) return;
+          if (newMinoState.x >= FieldWidth) return;
+          break;
+        case Input.RotateLeft:
+          newMinoState.rotation = ((newMinoState.rotation - 1) % 4) as Rotation;
+          break;
+        case Input.RotateRight:
+          newMinoState.rotation = ((newMinoState.rotation + 1) % 4) as Rotation;
+          break;
+        case Input.Down:
+          newMinoState.y += 1;
+          break;
+      }
+      const newField = field.map((row) => row.map((cell) => cell));
+      const shape = minoShapeMap.get(currentMino.mino);
+      if (!shape) return;
+      for (const [y, row] of shape.entries()) {
+        for (const [x, cell] of row.entries()) {
+          if (cell === Mino.None) continue;
+          const newY = newMinoState.y + y;
+          const newX = newMinoState.x + x;
+          if (newY >= FieldHeight || field[newY][newX] !== Mino.None) {
+            if (input !== Input.Down) return;
+            if (currentMino.y < 0) {
+              console.log("Game Over");
+              setField(initField);
+              return;
+            }
+            setField(displayField);
+            releaseMino();
             return;
+          } else {
+            newField[newY][newX] = cell;
           }
-          setField(displayField);
-          releaseMino();
-          return;
-        } else {
-          newField[newY][newX] = cell;
         }
       }
-    }
-    setCurrentMino(newMinoState);
-    setDisplayField(newField);
-  };
+      setCurrentMino(newMinoState);
+      setDisplayField(newField);
+    },
+    [currentMino, field, displayField, releaseMino]
+  );
 
   useEffect(() => {
     releaseMino();
     setInterval(() => setTick(Math.random()), 33);
-    setInterval(() => setInputQueue([...inputQueue, Input.Down]), 50);
-    setInterval(
-      () =>
-        setInputQueue([
-          ...inputQueue,
-          [Input.Left, Input.Right][Math.round(Math.random())],
-        ]),
-      500
-    );
+    setInterval(() => setInputQueue((prev) => [...prev, Input.Down]), 100);
   }, []);
 
   useEffect(() => {
     if (inputQueue.length === 0) return;
     moveMino(inputQueue.shift() as Input);
   }, [tick]);
+
+  useEffect(() => {
+    if (isReleased === false) return;
+    decisionInput();
+  }, [isReleased]);
 
   return (
     <>
